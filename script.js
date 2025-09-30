6002eb996129ce511e936f3311816e82f6d58a04
san4b0t <sanfo.bt@gmail.com>
2025-09-30 14:36:17 +0800

    Add dashboard contents

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ hamburger.addEventListener('click', function() {
     toggler.classList.toggle("bxs-chevrons-right");
     toggler.classList.toggle("bxs-chevrons-left");
 })
+
+new Chart(document.getElementById("bar-chart-grouped"), {
+    type: 'bar',
+    data: {
+      labels: ["1900", "1950", "1999", "2050"],
+      datasets: [
+        {
+          label: "Africa",
+          backgroundColor: "#3e95cd",
+          data: [133,221,783,2478]
+        }, {
+          label: "Europe",
+          backgroundColor: "#8e5ea2",
+          data: [408,547,675,734]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Population growth (millions)'
+      }
+    }
+});
